f9fb5cc6b9226650c50563085393e3ea731e9bad
StockWrapInfo <gugumgvgv@stockwrapinfo.com>
2018-03-25 01:46:03 +0530

    test_again

--- js/firebaseapp.js
+++ js/firebaseapp.js
@@ -1,4 +1,7 @@
-
+// TODO: Replace with your project's config object. You can find this
+// by navigating to your project's console overview page
+// (https://console.firebase.google.com/project/your-project-id/overview)
+// and clicking "Add Firebase to your web app"
 var config = {
   apiKey: "AIzaSyCqV6kTTwa0E6n7qg5my2vwHPP2-IGIvaw",
   authDomain: "stockwrapinfo-5ec9e.firebaseapp.com",
@@ -7,29 +10,26 @@ var config = {
   storageBucket: "",
   messagingSenderId: "737116197699"
 };
+
+// Initialize your Firebase app
 firebase.initializeApp(config);
 
-
 // Reference to the recommendations object in your Firebase database
-var user_data = firebase.database().ref("user_data");
+var recommendations = firebase.database().ref("recommendations");
 
 // Save a new recommendation to the database, using the input in the form
-var submitUser_data = function () {
+var submitRecommendation = function () {
 
   // Get input values from each of the form elements
-  var fname = $("#fname").val();
-  var sname = $("#sname").val();
-  var email = $("#exampleInputEmail1").val();
-  var ccode = $("#ccode").val();
-  var cnumber = $("#cnumber").val();
-  // Push a new recommendation to the database using those values
-  user_data.push({
-    "First_Name": fname,
-    "Second_Name": sname,
-    "MailID": email,
-    "Country_Code": ccode,
-    "Contact_Number": cnumber
+  var title = $("#fname").val();
+  var presenter = $("#exampleInputEmail1").val();
+  var link = $("#cnumber").val();
 
+  // Push a new recommendation to the database using those values
+  recommendations.push({
+    "title": title,
+    "presenter": presenter,
+    "link": link
   });
 };
 
@@ -42,6 +42,6 @@ $(window).load(function () {
 
   // Find the HTML element with the id recommendationForm, and when the submit
   // event is triggered on that element, call submitRecommendation.
-  $("#user_dataForm").submit(submitUser_data);
+  $("#user_dataForm").submit(submitRecommendation);
 
 });
